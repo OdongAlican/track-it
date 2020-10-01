64e6ad11d95dc5a343608b71de0523e21fbb98d9
OdongAlican <sandieo.2020@gmail.com>
2020-10-01 17:24:58 +0300

    add the schema file

---
+++ db/schema.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20_201_001_082_614) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension 'plpgsql'
+
+  create_table 'activities', force: :cascade do |t|
+    t.string 'title'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
+
+  create_table 'measurements', force: :cascade do |t|
+    t.decimal 'duration'
+    t.string 'date'
+    t.bigint 'activity_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['activity_id'], name: 'index_measurements_on_activity_id'
+  end
+
+  add_foreign_key 'measurements', 'activities'
+end
