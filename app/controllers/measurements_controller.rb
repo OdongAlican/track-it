9cceecb868717fd018f67fa7f9fc0205548a564d
OdongAlican <sandieo.2020@gmail.com>
2020-10-02 12:24:50 +0300

    add the bcrypt and cors file in the gemfile

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,43 +1,43 @@
 # frozen_string_literal: true
 
 class MeasurementsController < ApplicationController
-    before_action :set_activity
-    before_action :set_activity_measurement, only: [:show, :update, :destroy]
+  before_action :set_activity
+  before_action :set_activity_measurement, only: %i[show update destroy]
 
-    def index
-        json_response(@activity.measurements)
-    end
+  def index
+    json_response(@activity.measurements)
+  end
 
-    def show
-        json_response(@measurement)
-    end
+  def show
+    json_response(@measurement)
+  end
 
-    def create
-        @activity.measurements.create!(measurement_params)
-        json_response(@activity, :created)
-    end
+  def create
+    @activity.measurements.create!(measurement_params)
+    json_response(@activity, :created)
+  end
 
-    def update
-        @measurement.update(measurement_params)
-        head :no_content
-    end
+  def update
+    @measurement.update(measurement_params)
+    head :no_content
+  end
 
-    def destroy
-        @measurement.destroy
-        head :no_content
-    end
+  def destroy
+    @measurement.destroy
+    head :no_content
+  end
 
-    private
+  private
 
-    def measurement_params
-        params.permit(:date, :duration)
-    end
+  def measurement_params
+    params.permit(:date, :duration)
+  end
 
-    def set_activity
-        @activity = Activity.find(params[:activity_id])
-    end
+  def set_activity
+    @activity = Activity.find(params[:activity_id])
+  end
 
-    def set_activity_measurement
-        @measurement = @activity.measurements.find_by!(id: params[:id]) if @activity
-    end
+  def set_activity_measurement
+    @measurement = @activity.measurements.find_by!(id: params[:id]) if @activity
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_001_082_614) do
+ActiveRecord::Schema.define(version: 20_201_001_212_206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20_201_001_082_614) do
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
     t.index ['activity_id'], name: 'index_measurements_on_activity_id'
+  end
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'username'
+    t.string 'password_digest'
+    t.integer 'age'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
   add_foreign_key 'measurements', 'activities'
