710fa5f20b16568d7cf5195b59a1b6bbcba8441c
OdongAlican <sandieo.2020@gmail.com>
2020-10-13 20:38:26 +0300

    add models test for activity

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -3,5 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { FactoryBot.build(:activity) }
+  it { should have_many(:measurements).dependent(:destroy) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:total) }
 end
