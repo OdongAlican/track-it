13ad8f9c0bdfcd1df5ee72952dc5f641a59b356c
OdongAlican <sandieo.2020@gmail.com>
2020-10-13 20:38:45 +0300

    add models test for measurement

--- spec/models/measurement_spec.rb
+++ spec/models/measurement_spec.rb
@@ -3,5 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Measurement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:activity) }
+  it { should validate_presence_of(:duration) }
+  it { should validate_presence_of(:date) }
 end
