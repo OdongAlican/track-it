12eae5aa11dae2be953bbf1e4137098b8d62d47a
OdongAlican <sandieo.2020@gmail.com>
2020-10-13 20:39:10 +0300

    add models test for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:age) }
 end
